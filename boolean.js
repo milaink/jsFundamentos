33d93f45cc2f8eb57938aba8037a8ec35308ab36
Camila M <camilaom89@gmail.com>
2022-02-25 01:26:44 -0300

    Fundamentos do js parte2

---
+++ boolean.js
@@ -0,0 +1,20 @@
+//false
+const nome1 = 5;
+const nome2 = 5;
+let cadastraA = true;
+
+console.log(nome1 === nome2);
+
+const text1 = "Tom";
+const text2 = "tom";
+
+console.log(text1 === text2);
+
+// padrões de nome em JS. Use camelCase pfv
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
